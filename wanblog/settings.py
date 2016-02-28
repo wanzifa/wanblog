2e618f7caac99dab92c954f6a08e92d115e3d887
wanzifa <773857626@qq.com>
2016-02-28 22:37:23 +0800

    update

--- wanblog/settings.py
+++ wanblog/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/wanblog/static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
